1391047c56fc46bd4a7a0e4c38dd41708cd5c5d9
Kyler England <kylerengland32@gmail.com>
2023-02-15 18:35:32 -0500

    Added some comments.

--- js/validate.js
+++ js/validate.js
@@ -1,16 +1,11 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//   validate();
-// })
-
-// export default function validate(){
-
-// };
-
 const newPassword = document.querySelector("#newPassword");
 const newPassword2 = document.querySelector("#newPassword2");
 const errorText = document.querySelector("#error-text");
 const myButton = document.querySelector("#button");
 
+// When the button to submit the form is submitted it calls this function.
+// If the passwords do not match, it throws up an error message and doesn't allow the submission to go through.
+// Otherwise it briefly describes a success message and allows the submission to continue.
 myButton.onclick = function () {
   if (newPassword.value != newPassword2.value) {
     errorText.style.display = "block";
