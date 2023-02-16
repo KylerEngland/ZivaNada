44428e96b9531825ac89e18d6b7083f7a0030686
Kyler England <kylerengland32@gmail.com>
2023-02-15 20:21:42 -0500

    US 23.2: Changed the name from announcements to events.

--- navbar.php
+++ navbar.php
@@ -37,7 +37,7 @@ session_start();
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.php">Objave</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.php">Događaji</a></li>
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="lokacija.php">Lokacija</a></li>
                 </ul>
             </div>
