f91e50f14eb38fee78e174b05a27f319ac26a7d4
Kyler England <kylerengland32@gmail.com>
2023-03-01 17:46:55 -0500

    Added comments to the tests as well as a test for updating events.

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -1,6 +1,10 @@
 <?php
 require_once('protected/DB.php');
 class EventTest extends \PHPUnit\Framework\TestCase{
+    /*
+    This test checks to see if posting an Event without a date properly outputs the TBD messsage. 
+    Also makes sure the rest of the data gets posted correctly too.
+    */
     public function testInsertNoDate(){
         $database = new DB;
         $database->insertPost(6,"PHP-UnitTest","Description","","11:59:59");
@@ -20,6 +24,10 @@ class EventTest extends \PHPUnit\Framework\TestCase{
         }
     }
 
+    /*
+    This test checks to see if posting an Event with a date properly outputs. 
+    Also makes sure the rest of the data gets posted correctly too.
+    */
     public function testInsertWithDate(){
         $eventDate = "2015-08-17";
         $database = new DB;
@@ -40,6 +48,10 @@ class EventTest extends \PHPUnit\Framework\TestCase{
         }
     }
 
+    /*
+    This test checks to see if posting an Event without a time properly outputs the TBD messsage. 
+    Also makes sure the rest of the data gets posted correctly too.
+    */
     public function testInsertNoTime(){
         $database = new DB;
         $database->insertPost(6,"PHP-UnitTest","Description","2015-08-17","");
@@ -59,6 +71,10 @@ class EventTest extends \PHPUnit\Framework\TestCase{
         }
     }
     
+    /*
+    This test checks to see if posting an Event with a time properly outputs. 
+    Also makes sure the rest of the data gets posted correctly too.
+    */
     public function testIncludeTime(){
         $eventTime = "11:59:59";
         $database = new DB;
@@ -78,6 +94,46 @@ class EventTest extends \PHPUnit\Framework\TestCase{
             $database->deletePost($id);
         }
     }
+
+    /* 
+    This function tests the updating functionality of the database. First it uploads a post to the database,
+    then it calls the showPosts function to get the ID of the post (since that is assigned by the database).
+    Then it calls the editPost function using that ID, and asserts that the post has been updated to the edited version.
+    */
+    public function testUpdatingEvent(){
+        $userID = "5";
+        $newTitle = "This is the replacement title.";
+        $newDesc = "This is the replacement description.";
+        $newDate = "2001-12-14";
+        $newTime = "10:00:00";
+        $postID = "";
+
+        $database = new DB;
+        $database->insertPost(6,"PHP-UnitTest","Description","2015-08-17","11:59:59");
+        $posts = $database->showPosts(1);
+
+        foreach($posts as $event){ 
+            $postID = $event->getPostID();
+        }
+        $database->editPost($postID,$userID, $newTitle, $newDesc, $newDate, $newTime);
+
+        $posts = $database->showPosts(1);
+        foreach($posts as $event){
+            $this->assertEquals(5, $event->getUserID());
+            $this->assertEquals("This is the replacement title.", $event->getTitle());
+            $this->assertEquals("This is the replacement description.", $event->getDescription());
+            $this->assertEquals("2001-12-14",$event->getEventDate());
+            $this->assertEquals("10:00:00",$event->getEventTime());
+        }
+
+        if($postID != ""){
+            $database->deletePost($postID);
+        }
+    }
+
+    // public function testDeletion(){
+
+    // }
 }
 
 ?>
