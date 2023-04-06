2c3fb01cbcbd351631eae8a1e6d031eee83303d3
Kyler England <kylerengland32@gmail.com>
2023-04-06 12:40:32 -0400

    Made the page redirect to values if you are trying to translate the events.

--- dogadaji.php
+++ dogadaji.php
@@ -38,8 +38,7 @@ $existingPosts = $database->getPostsNumber();
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
                     <div class="site-heading">
-                        <h1>Živa Nada</h1>
-                        <span class="subheading">Kristova Crkva</span>
+                        <h1>Događaji</h1>
                     </div>
                 </div>
             </div>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,29 +1,36 @@
 <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-        <div class="container px-4 px-lg-5">
-            <img src="assets/img/DoveColorLogo.png" alt="" style="width: auto; height: 50px">
-            <a class="navbar-brand" href="index.php">Živa Nada</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="dogadaji.php">Događaji</a></li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="vrijednosti.php">Temeljne Vrijednosti</a></li>
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="lokacija.php">Lokacija</a></li>
-                </ul>
-                <div class="dropdown">
+<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+    <div class="container px-4 px-lg-5">
+        <img src="assets/img/DoveColorLogo.png" alt="" style="width: auto; height: 50px">
+        <a class="navbar-brand" href="index.php">Živa Nada</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="dogadaji.php">Događaji</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="vrijednosti.php">Temeljne
+                        Vrijednosti</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="lokacija.php">Lokacija</a></li>
+            </ul>
+            <div class="dropdown">
                 <a class="btn btn-outline-secondary dropdown-toggle btn-sm" href="#" role="button" id="dropdownMenuLink"
                     data-bs-toggle="dropdown" aria-expanded="false">
                     <img src="assets/img/Flags/hr.svg" alt="" style="height:1.5em; width:auto;">
                 </a>
 
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <li><a class="dropdown-item" href="en/<?php echo basename($_SERVER['PHP_SELF']);?>"><img src="assets/img/Flags/us.svg" alt="" style="height:1.5em; width:auto;">&ensp;English</a></li>
+                    <li><a class="dropdown-item" href="en/<?php if (basename($_SERVER['PHP_SELF']) == "dogadaji.php") {
+                        echo ("vrijednosti.php");
+                    } else {
+                        echo basename($_SERVER['PHP_SELF']);
+                    } ?>"><img src="assets/img/Flags/us.svg" alt="" style="height:1.5em; width:auto;">&ensp;English</a>
+                    </li>
+
                 </ul>
             </div>
-            </div>
         </div>
-    </nav>
+    </div>
+</nav>
