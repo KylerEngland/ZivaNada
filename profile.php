554bef79dfdd23292c996b425d92fedb5b908f2d
KylerEngland <80475428+KylerEngland@users.noreply.github.com>
2024-02-25 15:32:04 -0700

    Added a couple backgrounds

--- profile.php
+++ profile.php
@@ -28,7 +28,7 @@ session_start();
 <body>
     <?php include_once('includes/navbar.php'); ?>
     <!-- Page Header-->
-    <header class="masthead" style="background-image: url('assets/img/birdseye.jpg')">
+    <header class="masthead" style="background-image: url('assets/img/Backgrounds/IMG_0131.jpg')">
         <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
